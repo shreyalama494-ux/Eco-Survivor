37df9c1ff6c06414b377a799a6756292d2b94c44
shreyalama494-ux <shreyalama494@gmail.com>
2026-05-13 17:10:02 +0545

    Wiped Assets for clean start

--- Assets/Script/TigerFootsteps.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class TigerFootsteps : MonoBehaviour
-{
-    // This is empty on purpose to stop the animation errors
-    public void OnFootstep(AnimationEvent animationEvent) { }
-    public void OnLand(AnimationEvent animationEvent) { }
-}
